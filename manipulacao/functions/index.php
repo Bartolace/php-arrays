20b41617e8e689f541f9c89701122d50d01de45f
gabriel <gabrielll.freitas@hotmail.com>
2024-03-27 13:35:00 -0400

    Update index.php

--- manipulacao/functions/index.php
+++ manipulacao/functions/index.php
@@ -45,9 +45,27 @@ $numeroDePaises = count($dadosEmJson);
 echo "Número de países participantes: $numeroDePaises";
 
 //Fazer com que seus nomes fiquem em letras maiúsculas;
-$dadosEmJson = array_map(function ($item) {
+$nomeUpper = array_map(function ($item) {
     $item['pais'] = mb_convert_case($item['pais'], MB_CASE_UPPER);
     return $item;
 }, $dadosEmJson);
 
+
+//Ordenar: Pelo país que tiver mais medalhas de ouro
+
+usort($dadosEmJson, function ($item1, $item2) {
+    $medalhas1 = $item1['medalhas'];
+    $medalhas2 = $item2['medalhas'];
+
+    return $medalhas2['ouro'] - $medalhas1['ouro'] !== 0
+        ? $medalhas2['ouro'] - $medalhas1['ouro']
+        : ($medalhas2['prata'] - $medalhas1['prata'] !== 0
+            ? $medalhas2['prata'] - $medalhas1['prata'] 
+            : $medalhas2['bronze'] - $medalhas1['bronze']);
+});
+
 var_dump($dadosEmJson);
+
+//Em caso de empate, pelo país que tiver mais medalhas de prata
+
+//Em caso de empate, pelo país que tiver mais medalhas de bronze
