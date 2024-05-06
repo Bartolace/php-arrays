94b45839d3628d6938a471ef52357f7bb9d5cea1
Bartolace <gabrielll.freitas@hotmail.com>
2024-05-06 13:24:25 -0400

    Update verificacoes.php

--- texto/verificacoes.php
+++ texto/verificacoes.php
@@ -1,4 +1,22 @@
 <?php
+$mail = 'gabríelll.freitas@hotmail.com';
 $nome= "Vinicius Dias Junior";
 $haDiasNaStr = str_contains($nome, "Dias");
-var_dump($haDiasNaStr);
+$comecaCom = str_starts_with($nome, "Vini");
+$terminaCom = str_ends_with($nome, "Juncior");
+
+
+
+$posicaoArroba = strpos($mail, '@');
+$extraiStrDepois = substr($mail, $posicaoArroba);
+$extraiStrAntes = substr($mail, 0, $posicaoArroba);
+//o mb vem de mult bit, serve para trabalhar com caracteres que ocupam mais de um bit
+//precisa ativar o mb no php ini
+$toUpper = mb_strtoupper($extraiStrAntes);
+$toLower = mb_strtolower($extraiStrAntes);
+
+
+$tamanhoStrBit = mb_strlen($mail);
+
+
+var_dump($tamanhoStrBit);
