47c39b91d8bc64af83eaa5692b622efe7da8263f
gabrielBartolace <gabrielll.freitas@hotmail.com>
2024-05-31 17:54:07 -0400

    Update alteracoes.php

--- texto/alteracoes.php
+++ texto/alteracoes.php
@@ -1,8 +1,15 @@
 <?php 
     $telefones = ['(24) 99999 - 9999', '(21) 99999 - 9999', '(24) 2222 - 2222'];
     $array = '1 2 3 4';
+    $email = ".,gabriel.freitas@hotmail.com,.";
+    $anoNascimento = '1998';
 
     $separandoPorAlgo = implode( PHP_EOL, $telefones);
-    $explodeComLimit = explode(' ', $array, 2);
-    print_r($explodeComLimit);  
+    $explodeStringEmArr = explode(' ', $array);
+    $removeEspacos = trim($email);
+    $removeAlgoDasPontas = trim($email, ".,");
+    $apararLeft = ltrim($email, ".");
+    $apararRigth = rtrim($email, ".");
+
+    print_r($apararLeft);  
 ?>
