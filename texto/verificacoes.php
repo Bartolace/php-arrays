dd019a9d2695cae5cb3ac0974fca283aa8bb2dcb
Gabriel Bartolace <86331019+Bartolace@users.noreply.github.com>
2024-08-27 00:55:40 -0400

    Update verificacoes.php

--- texto/verificacoes.php
+++ texto/verificacoes.php
@@ -5,12 +5,10 @@ $haDiasNaStr = str_contains($nome, "Dias");
 $comecaCom = str_starts_with($nome, "Vini");
 $terminaCom = str_ends_with($nome, "Juncior");
 
-
-
 $posicaoArroba = strpos($mail, '@');
 $extraiStrDepois = substr($mail, $posicaoArroba);
 $extraiStrAntes = substr($mail, 0, $posicaoArroba);
-//o mb vem de mult bit, serve para trabalhar com caracteres que ocupam mais de um bit
+//o mb vem de multbit, serve para trabalhar com caracteres que ocupam mais de um bit
 //precisa ativar o mb no php ini
 $toUpper = mb_strtoupper($extraiStrAntes);
 $toLower = mb_strtolower($extraiStrAntes);
